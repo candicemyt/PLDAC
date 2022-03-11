53abd25d7379d824670f1528fb9bfea2a2e55da5
Candice <candicemyt>
2022-03-11 15:43:09 +0100

    nouveaux fichiers qr codes

--- generation_qrcode/generate_qr_codes.py
+++ generation_qrcode/generate_qr_codes.py
@@ -174,8 +174,6 @@ def draw_qrcodes(qr_codes,outline, spacing):
 
 
 
-
-
 ####################### generation qr_codes optimaux ############################
 
 
@@ -183,7 +181,6 @@ id_qr_code, qr_codes = binary_qrcode(3, 8)
 outline = 2
 #espacements : grand = (2,2), moyen = (1, 1.8), petit = (0.5, 1.6)
 draw_qrcodes(qr_codes[1::], outline=outline,spacing=(1,1.8))
-
 
 
 ############ etude des qr codes optimaux #############
