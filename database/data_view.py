56b547f69b1a7a1503b768c531553b175c62e522
ManonLefevre <you@example.com>
2022-04-11 16:08:42 +0200

    suppression dossier matlab

---
+++ database/data_view.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data=pd.read_csv('jaguar_movement_data.csv',parse_dates=True,index_col='timestamp',usecols=[1,2,3,6])
+print(data.head())
+print(data.index)
