adfbd3930e7ba6402f9bbd6005acc63b5e54d9be
Candice <candicemyt>
2022-04-20 11:09:29 +0200

    premier tests de kmeans + jaguar

--- database/data_view.py
+++ database/data_view.py
@@ -1,7 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from tslearn.utils import to_time_series
+from tslearn.clustering import TimeSeriesKMeans
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv('jaguar_movement_data.csv',parse_dates=True,index_col='timestamp',usecols=[1,2,3,6])
-print(data.head())
-print(data.index)
+print(data.columns)
+data_i1 = data[data['individual.local.identifier (ID)'] == 1]
+data_i1.pop('individual.local.identifier (ID)')
+print(data_i1)
+#X_train, y_train, X_test, y_test = train_test_split(data)
+# km = TimeSeriesKMeans(n_clusters=3, metric="dtw", max_iter=10)
+# km.fit(data[0:1000])
+# pred = km.predict(data[1001:1101])
+# print(pred)
+#
--- detection_tag/main.py
+++ detection_tag/main.py
@@ -9,11 +9,3 @@ for mat in liste_mat:
     if _id > -1:
         qr_codes.append(_id)
 print(qr_codes)
-
-
-# c= [[0,0,1,1],[1,0,1,1],[1,1,1,1],[0,0,0,0],[1,1,1,1]]
-# print(analyse(c))
-# new_c = np.flip(c).tolist()
-# print(analyse(new_c))
-# c= [[1, 0, 0, 0], [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0]]
-# print(analyse(c))
