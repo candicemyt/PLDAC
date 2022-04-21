9d18a1a63fa72920763108c9c1c839aa92c04ae4
Candice <candicemyt>
2022-04-21 13:56:04 +0200

    recuperation data test

--- database/data_view.py
+++ database/data_view.py
@@ -4,16 +4,49 @@ import pandas as pd
 
 
 df = pd.read_csv('data_ant.csv')
+print(df.columns)
 df = df.rename(columns={"file_name": "num_frame"})
+df['num_frame'] = df['num_frame']/30
+dates = set(df['date'])
+colonies = set(df['colony'])
+data = []
+for d in dates:
+    df_date = df[df['date'] == d]
+    df_date.pop('date')
+    for c in colonies:
+        df_col = df_date[df_date['colony'] == c]
+        df_col.pop('colony')
+        data.append(df_col)
+print(data[0])
 
-df1 = df[df['colony'] == 'N2']
-df1.pop('colony')
-df2 = df[df['colony'] == 'N13']
-df2.pop('colony')
-df3 = df[df['colony'] == 'N4']
-df3.pop('colony')
 
-#todo merge les colonnes date et num_frame pour avoir des timestamp exploitables
-df1_ant1 = df1[df1['ant_id'] == 1]
-df1_ant1.sort_values(by=['num_frame'], ascending=False)
-print(df1_ant1)
+
+# #todo merge les colonnes date et num_frame pour avoir des timestamps exploitables
+# df1_ant1 = df1[df1['ant_id'] == 1]
+# df1 = df1.sort_values(by=['num_frame'], ascending=True)
+# df1['num_frame'] = df1['num_frame']/30
+# print(df1)
+
+
+#xls = pd.ExcelFile()
+# data = []
+# for i in range(1,19):
+#     file = pd.read_excel('data_ant.xlsx', f"m{i}")
+#     file.to_csv(f"data/m{i}.csv", header=True)
+#     df = pd.read_csv(f"data/m{i}.csv")
+#     df = df.rename(columns={"file_name": "num_frame"})
+#     print(df.columns)
+#     print(df.head())
+#     data.append(df)
+
+
+# file = pd.read_excel('m1.xlsx')
+# file.to_csv('m1.csv', header=True)
+# df = pd.read_csv('m1.csv')
+# print(df.columns)
+#
+#
+# file = pd.read_excel('m2.xlsx')
+# file.to_csv('m2.csv', header=True)
+# df = pd.read_csv('m2.csv')
+# print(df.columns)
